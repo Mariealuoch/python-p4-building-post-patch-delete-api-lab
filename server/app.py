68dc2003bd87f76526f075e91875737b9a19bf82
TITAN <aluochmarion7@gmail.com>
2024-06-28 23:18:43 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -18,17 +18,25 @@ db.init_app(app)
 def home():
     return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
 
-@app.route('/bakeries')
-def bakeries():
-    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
-    return make_response(  bakeries,   200  )
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    new_baked_good = BakedGood(
+        name=data['name'],
+        price=float(data['price']),
+        bakery_id=int(data['bakery_id'])
+    )
+    db.session.add(new_baked_good)
+    db.session.commit()
 
+    return make_response(jsonify(new_baked_good.to_dict()), 201)
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
 
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
     return make_response ( bakery_serialized, 200  )
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -38,6 +46,22 @@ def baked_goods_by_price():
     ]
     return make_response( baked_goods_by_price_serialized, 200  )
    
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get_or_404(id)
+    db.session.delete(baked_good)
+    db.session.commit()
+    return make_response(jsonify({"message": "Baked good successfully deleted"}), 200)
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.get_or_404(id)
+    data = request.form
+    if 'name' in data:
+        bakery.name = data['name']
+        db.session.commit()
+    return make_response(jsonify(bakery.to_dict()), 200)
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
